046b58b5f109eadb2162a4a3ffb893da9e9696a7
holoflash <mikepalacemusic@gmail.com>
2023-04-12 09:53:01 +0200

    Refactor backend code

---
+++ backend/mongoAPI/mongo.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import { Party } from './PartySchema.js';
+
+const router = express.Router();
+
+router.post('/api/party', async (req, res) => {
+    try {
+        const party = new Party(req.body)
+        const result = await party.save();
+        res.json(result)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+router.put('/api/party/:id/suggestions/:suggested_by', async (req, res) => {
+    const { id, suggested_by } = req.params;
+
+    try {
+        const party = await Party.findOne({ _id: id });
+        if (!party) {
+            return res.status(404).json({ message: 'Party not found' });
+        }
+
+        await Party.updateOne(
+            { _id: id, 'members.name': suggested_by },
+            { $inc: { 'members.$.songs_to_suggest': -1 }, $set: { 'members.$.is_done': true } }
+        );
+
+        party.suggestions.push(req.body);
+        await party.save();
+
+        res.status(200).json({ message: 'Suggestion added successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.get('/api/party/:id', async (req, res) => {
+    try {
+        const party = await Party.findById(req.params.id);
+        if (!party) {
+            return res.status(404).json({ message: 'Party not found' });
+        }
+        res.json(party);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.delete('/api/party/:id', async (req, res) => {
+    try {
+        const party = await Party.findById(req.params.id);
+        if (!party) {
+            return res.status(404).json({ message: 'Party not found' });
+        }
+        await Party.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Party deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+export { router as mongoRouter };
---
+++ backend/spotifyAPI/spotify.js
@@ -0,0 +1,156 @@
+import express from 'express';
+import querystring from 'querystring';
+import axios from 'axios';
+import { generateRandomString } from './generateRandomString.js';
+
+const router = express.Router();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const stateKey = 'spotify_auth_state';
+
+router.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = `user-read-private user-read-email ugc-image-upload playlist-modify-private playlist-modify-public`;
+
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        state: state,
+        scope: scope,
+    });
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`)
+})
+
+router.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+    const authString = `${CLIENT_ID}:${CLIENT_SECRET}`;
+    const base64Auth = Buffer.from(authString).toString('base64');
+    const authHeader = `Basic ${base64Auth}`;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: authHeader
+        },
+    })
+        .then(response => {
+            if (response.status === 200) {
+                const { access_token, refresh_token, expires_in } = response.data;
+
+                const queryParams = querystring.stringify({
+                    access_token,
+                    refresh_token,
+                    expires_in,
+                });
+
+                res.redirect(`http://localhost:3000/?${queryParams}`);
+
+            } else {
+                res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
+            }
+        })
+        .catch(error => {
+            res.send(error);
+        });
+});
+
+router.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+    const authString = `${CLIENT_ID}:${CLIENT_SECRET}`;
+    const base64Auth = Buffer.from(authString).toString('base64');
+    const authHeader = `Basic ${base64Auth}`;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: authHeader
+        },
+    })
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            res.send(error);
+        });
+});
+
+async function getAccessToken() {
+    const authString = `${CLIENT_ID}:${CLIENT_SECRET}`;
+    const base64Auth = Buffer.from(authString).toString('base64');
+    const authHeader = `Basic ${base64Auth}`;
+
+    const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'client_credentials'
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: authHeader
+        }
+    });
+    return response.data.access_token;
+}
+
+//SPOTIFY song search
+router.get('/api/search/:query', async (req, res) => {
+    const query = req.params.query;
+    const accessToken = await getAccessToken();
+    try {
+        const response = await axios({
+            method: 'get',
+            url: `https://api.spotify.com/v1/search?q=track:${query}&type=track&market=US`,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'content-type': 'application/json',
+            }
+        });
+        const data = response.data;
+        res.json(data)
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+//SPOTIFY create a playlist
+router.post('/api/create-playlist/:user_id', async (req, res) => {
+    const user_id = req.params.user_id;
+    const token = req.headers.authorization.split(' ')[1];
+    const body = req.body;
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `https://api.spotify.com/v1/users/${user_id}/playlists`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'content-type': 'application/json',
+            },
+            data: body,
+        });
+        const data = response.data;
+        res.json(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+export { router as spotifyRouter };
