d98fcb60034c11ef31a1e48afee189bd8a57cd8e
holoflash <mikepalacemusic@gmail.com>
2023-04-26 11:13:46 +0200

    Add more error handling in server

--- backend/mongoAPI/db.js
+++ backend/mongoAPI/db.js
@@ -2,7 +2,11 @@ import { MongoClient } from 'mongodb';
 import 'dotenv/config'
 
 export async function connectToDb(cb) {
-    const client = new MongoClient(process.env.ATLAS_URI);
-    await client.connect();
-    cb();
+    try {
+        const client = new MongoClient(process.env.ATLAS_URI);
+        await client.connect();
+        cb();
+    } catch (error) {
+        console.error('Failed to connect to database:', error);
+    }
 }
