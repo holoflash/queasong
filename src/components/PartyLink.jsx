09f197bb9c3675cdafaece01d024d0a43be28988
holoflash <mikepalacemusic@gmail.com>
2023-04-26 12:38:09 +0200

    Final url fix

--- src/components/PartyLink.jsx
+++ src/components/PartyLink.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react';
 
 export const PartyLink = ({ party_member, party_id }) => {
     const [linkCopied, setLinkCopied] = useState(false);
-    const URL = `${process.env.FRONTEND_URI}/${party_member}/${party_id}`;
+    const URL =
+        process.env.NODE_ENV !== 'production'
+            ? 'http://localhost:8888'
+            : 'https://queasong.onrender.com';
+
+
+    const fullUrl = `${URL}/${party_member}/${party_id}`;
 
     function copyToClipboard(text) {
         navigator.clipboard.writeText(text).then(() => {
@@ -15,12 +21,12 @@ export const PartyLink = ({ party_member, party_id }) => {
             <label>
                 <input
                     type="text"
-                    value={URL}
+                    value={fullUrl}
                     readOnly={true}
                     style={{ border: '1px solid #ccc', padding: '0.5em' }}
                 />
 
-                <button id="copy-link-button" onClick={() => copyToClipboard(URL)}>
+                <button id="copy-link-button" onClick={() => copyToClipboard(fullUrl)}>
                     COPY TO CLIPBOARD
                 </button>
             </label>
