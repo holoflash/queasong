465c7813fcdef26b0460998fa97458b05d186cad
holoflash <mikepalacemusic@gmail.com>
2023-04-26 12:26:55 +0200

    Troubleshooting

--- src/components/HostLogIn.jsx
+++ src/components/HostLogIn.jsx
@@ -1,10 +1,13 @@
 export const HostLogin = () => {
-    const LOGIN = process.env.LOGIN_URI
+    const LOGIN_URI =
+        process.env.NODE_ENV !== 'production'
+            ? 'http://localhost:8888/login'
+            : 'https://queasong.onrender.com/login';
     return (
         <div id="login">
             <button
                 onClick={() =>
-                    (window.location.href = `${LOGIN}`)
+                    (window.location.href = `${LOGIN_URI}`)
                 }
             >
                 Log in to Spotify
